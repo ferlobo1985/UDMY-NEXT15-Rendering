85872831a794d667f3816882ee9028e8bf838101
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-27 00:04:10 -0300

    08-Server form - no js
    08-Server form - no js

--- src/helpers/actions.js
+++ src/helpers/actions.js
@@ -1,5 +1,14 @@
 'use server'
+import axios from "axios";
 
 export async function counterTrigger() {
     console.log('COUNTER TRIGGER');
+}
+
+export async function addEmployee(formdata){
+    await axios.post('http://localhost:3004/employees',{
+      fullname: formdata.get('fullname'),
+      position: formdata.get('position'),
+      age: formdata.get('age'),
+    })
 }
