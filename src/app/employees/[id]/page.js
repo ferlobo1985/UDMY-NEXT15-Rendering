e3423b84e992a098e19ec907d417d740f38ac4b1
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-27 00:58:13 -0300

    11-Employee by ID
    11-Employee by ID

--- src/app/employees/[id]/page.js
+++ src/app/employees/[id]/page.js
@@ -1,8 +1,17 @@
-export default async function EmployeeByID() {
-    return (
-     <>
-      <h1>By ID employee</h1>
-     </>
-    );
-  }
+import axios from 'axios'
+
+export default async function EmployeeByID({params}) {
+  const ID = (await params).id;
+  const employee = await axios.get(`http://localhost:3004/employees/${ID}`);
+
+  return (
+    <>
+      <h1>{employee.data.fullname}</h1>
+      <div>
+        <h4>Position:{employee.data.position}</h4>
+        <h4>Age:{employee.data.age}</h4>
+      </div>
+    </>
+  );
+}
   
