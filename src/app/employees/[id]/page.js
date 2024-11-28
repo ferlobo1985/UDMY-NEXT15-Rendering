4234e39cc1ecd3654236f384c904510eca6d6ab0
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-28 05:39:10 -0300

    21-Segment config options
    21-Segment config options

--- src/app/employees/[id]/page.js
+++ src/app/employees/[id]/page.js
@@ -2,6 +2,8 @@ import { notFound } from "next/navigation";
 import Link from 'next/link';
 import { deleteEmployee } from "@/helpers/actions";
 
+// export const dynamic = 'force-static';
+
 async function getEmployee(ID){
   const res = await fetch(`http://localhost:3004/employees/${ID}`);
   if(!res.ok){
