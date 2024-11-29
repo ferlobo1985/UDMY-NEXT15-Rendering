57478590f3a65dca5efa7710fe1d6c5f0485840f
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-29 05:10:10 -0300

    28-GET employees
    28-GET employees

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,8 +1,21 @@
-export async function GET(request){
-    // console.log(request)
-    const x = request.nextUrl.searchParams.get('x');
-    const y = request.nextUrl.searchParams.get('y');
+import { cookies, headers } from "next/headers";
+
+export async function GET(){
+    const cookieStore = cookies();
+    const token = (await cookieStore).get('token');
+
+    const auth = (await headers()).get('Authorization'); 
+    const type = (await headers()).get('Content-Type'); 
+    console.log(auth, type)
 
 
-    return Response.json({x,y})
+    const res = await fetch('http://localhost:3004/employees',{
+        next:{revalidate:30}
+    });
+    const employees = await res.json();
+
+    return Response.json(employees,{
+        status:200,
+        headers:{'Set-cookie':'token=Bearer jksbfbgksdbfh'}
+    })
 }
